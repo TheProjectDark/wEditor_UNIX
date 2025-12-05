89d0dc951f96523f16480942042e20b5ec6782cd
Vladlen <v.shapoval.7@gmail.com>
2025-12-05 08:55:48 +0100

    Added scalability, class MainFrame was split from main file

---
+++ MainFrame.h
@@ -0,0 +1,17 @@
+#pragma once
+#include <wx/wx.h>
+
+//creating the frame class with its functions
+class MainFrame : public wxFrame
+{
+    public:
+        MainFrame(const wxString& title);
+
+    private:
+        wxTextCtrl* textCtrl;
+
+        void OnExit(wxCommandEvent& event);
+        void OnAbout(wxCommandEvent& event);
+        void OnSave(wxCommandEvent& event);
+        void OnOpen(wxCommandEvent& event);
+};
--- main.cpp
+++ main.cpp
@@ -11,21 +11,7 @@
 #include <wx/wx.h>
 #include <wx/filedlg.h>
 #include <wx/file.h>
-
-//creating the frame class with its functions
-class MainFrame : public wxFrame
-{
-    public:
-        MainFrame(const wxString& title);
-
-    private:
-        wxTextCtrl* textCtrl;
-
-        void OnExit(wxCommandEvent& event);
-        void OnAbout(wxCommandEvent& event);
-        void OnSave(wxCommandEvent& event);
-        void OnOpen(wxCommandEvent& event);
-};
+#include "MainFrame.h"
 
 //app class to launch this fuckass editor
 class App : public wxApp
@@ -35,20 +21,31 @@ class App : public wxApp
 };
 
 //the main part of the code
-MainFrame::MainFrame(const wxString& title): wxFrame(nullptr, wxID_ANY, title) {
+MainFrame::MainFrame(const wxString& title)
+    : wxFrame(nullptr, wxID_ANY, title)
+{
     wxPanel* panel = new wxPanel(this);
 
     wxMenu *menuHelp = new wxMenu;
     menuHelp->Append(wxID_ABOUT);
- 
+
     wxMenuBar *menuBar = new wxMenuBar;
     menuBar->Append(menuHelp, "&Help");
-
     SetMenuBar(menuBar);
 
-    textCtrl = new wxTextCtrl(panel, wxID_ANY, "", wxPoint(10, 50), wxSize(780, 540), wxTE_MULTILINE);
-    wxButton* save = new wxButton(panel, wxID_ANY, "Save", wxPoint(10, 10), wxSize(40, 30));
-    wxButton* open = new wxButton(panel, wxID_ANY, "Open", wxPoint(60, 10), wxSize(50, 30));
+    textCtrl = new wxTextCtrl(panel, wxID_ANY, "", wxDefaultPosition, wxDefaultSize, wxTE_MULTILINE);
+    wxButton* save = new wxButton(panel, wxID_ANY, "Save");
+    wxButton* open = new wxButton(panel, wxID_ANY, "Open");
+    wxBoxSizer* mainSizer = new wxBoxSizer(wxVERTICAL);
+    wxBoxSizer* buttonSizer = new wxBoxSizer(wxHORIZONTAL);
+
+    buttonSizer->Add(save, 0, wxRIGHT, 5);
+    buttonSizer->Add(open, 0);
+    mainSizer->Add(buttonSizer, 0, wxALL | wxALIGN_LEFT, 5);
+    mainSizer->Add(textCtrl, 1, wxEXPAND | wxALL, 5);
+
+    panel->SetSizer(mainSizer);
+    mainSizer->SetSizeHints(this);
 
     save->Bind(wxEVT_BUTTON, &MainFrame::OnSave, this);
     open->Bind(wxEVT_BUTTON, &MainFrame::OnOpen, this);
@@ -62,7 +59,7 @@ bool App::OnInit() {
     SetExitOnFrameDelete(true);
 
     MainFrame* mainFrame = new MainFrame("wEditor");
-    mainFrame->SetClientSize(800, 600);
+    mainFrame->SetClientSize(mainFrame->FromDIP(wxSize(800, 600)));
     mainFrame->Show();
     return true;
 }
